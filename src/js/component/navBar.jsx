38275082d27b193c43bde36c4d934ff2b4bddcd0
Frcru <91342407+Frcru@users.noreply.github.com>
2024-08-09 23:51:54 +0000

    NavBar's text sizes has change

--- src/js/component/navBar.jsx
+++ src/js/component/navBar.jsx
@@ -7,11 +7,11 @@ const NavBar = () => {
 
         <nav className="navbar navbar-expand-lg bg-body-tertiary bg-black ">
             <div className="container-fluid position-relative">
-                <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+                <a className="navbar-brand text-light fw-bold fs-2" href="#">Start Bootstrap</a>
                 <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse position-absolute top-0 end-0" id="navbarText">
+                <div className="collapse navbar-collapse position-absolute bottom-0 end-0 fs-4" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 me-3">
                         <li className="nav-item me-3">
                             <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
