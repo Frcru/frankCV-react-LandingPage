c9bcff5217eed5d844c7ca4a53c41eed366ee021
Frcru <91342407+Frcru@users.noreply.github.com>
2024-08-09 23:45:59 +0000

    deleted unnecesary  d-flex

--- src/js/component/app.jsx
+++ src/js/component/app.jsx
@@ -6,13 +6,13 @@ const App = () => {
     //This is a form to create an array with any amount of elements, in this case is 4 (length:4)
     //Array.from(length,(element,index)=>())    note that parenthesis is after arrow =>  so if you put it with keys {}
     // this won't render anything 
-    const repeat = Array.from({ length: 4 }, (_, index) => (<Card key={index}/>)
+    const repeat = Array.from({ length: 4 }, (_, index) => (<Card key={index} />)
     );
     //to use for, we will need to create an array
     let repeated = [];
     for (let index = 0; index < 4; index++) {
         repeated.push(
-           <Card key={index}/>
+            <Card key={index} />
         )
 
     }
@@ -22,12 +22,11 @@ const App = () => {
             <div className="container-fluid w-75">
                 <Landing />
             </div>
-            <div className="container-fluid w-75 d-flex">
+            <div className="container-fluid w-75">
                 {/* remember, here isn't necessary to put () after repeat */}
                 <div className="row">
                     {repeat}
                 </div>
-
             </div>
 
         </div>
--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-
+//Remember to specify breakpoints (sm,md,lg,xl,xxl) with the columns that you want to fill
 const Card = () => {
     return (
-        <div className="col-3">
+        <div className="col-xl-6 col-xxl-3 col-lg-6 col-md-6 col-sm-12">
             <div className="card mt-4 me-3" style={{ width: '100%', borderRadius: '0px' }}>
                 <img src="https://placehold.it/500x325" className="img-fluid" alt="..." />
                 <div className="card-body d-flex flex-column">
